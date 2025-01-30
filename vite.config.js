0a9d02f9aa7f9c5fd56d0b8fa750dd66243c2d61
Pedro Andrade Jiménez <pedroandradejimenez@MacBook-Air-de-Pedro.local>
2025-01-30 17:41:38 -0300

    Eliminacion de codigo que apuntaba a heroku

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,13 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001', // Usar process.env
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // Eliminar o comentar la configuración del proxy
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.VITE_API_URL || 'http://localhost:5001',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
 });
